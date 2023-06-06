28e2ae03ac31aa276ad31b7f48cb1b0739d04458
flaviozorzetto <flaviozorzetto11@gmail.com>
2023-06-06 02:28:47 -0300

    first commit

---
+++ src/main/java/br/com/fiap/gsproject/GsprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/gsproject/GsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
